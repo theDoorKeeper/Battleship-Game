02830f83fd824cf6a5c6ad58a5eb0dbc375946d6
thteDoorKeeper <chakibou1@hotmail.com>
2021-07-01 18:37:44 +0100

    added 2 new cases (mutliple index array hit&miss ) for ship factory

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -1,7 +1,7 @@
 import { createShip } from "../JS/ship";
 
 
-test('Works with single index arrays', () => {
+test('Works with single index arrays (hit)', () => {
     const coordinates = [25];
     const name = "submarine" ;
     const length = 1;
@@ -10,7 +10,7 @@ test('Works with single index arrays', () => {
     expect( submarine.isSunk() ).toBe( true );
   });
 
-  test('Works with single index arrays', () => {
+  test('Works with single index arrays (miss) ', () => {
     const coordinates = [28];
     const name = "submarine" ;
     const length = 1;
@@ -19,5 +19,22 @@ test('Works with single index arrays', () => {
     expect( submarine.isSunk() ).toBe( false );
   });
 
+  test('Works with multiple index arrays (hit)', () => {
+    const coordinates = [25,24,23];
+    const name = "submarine" ;
+    const length = 1;
+    const submarine = createShip(name,length,coordinates);
+    submarine.hit(23);
+    expect( submarine.isSunk() ).toBe( true );
+  });
+
+  test('Works with multiple index arrays (miss)', () => {
+    const coordinates = [25,24,23];
+    const name = "submarine" ;
+    const length = 1;
+    const submarine = createShip(name,length,coordinates);
+    submarine.hit(29);
+    expect( submarine.isSunk() ).toBe( false );
+  });
 
   
