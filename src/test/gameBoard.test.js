1c9470f707bd9e781f6d16e9a3e78f2a77267f8d
thteDoorKeeper <chakibou1@hotmail.com>
2021-07-02 19:20:44 +0100

    added test case for gameBoard

--- src/test/gameBoard.test.js
+++ src/test/gameBoard.test.js
@@ -1,1 +1,13 @@
 import { createGameboard } from "../JS/gameboard";
+
+
+test('Works with single index arrays (hit)', () => {
+    const coordinates = [25];
+    const name = "submarine" ;
+    const testingGameboard = createGameboard();
+    testingGameboard.placeShip("submarine",22)
+    testingGameboard.receiveAttack(22);
+    testingGameboard.receiveAttack(23);
+
+    expect( testingGameboard.isGameLost() ).toBe( true );
+  });
