231b96e38f5648e2c710f110bf8baf5e4d632789
thteDoorKeeper <chakibou1@hotmail.com>
2021-07-02 16:33:15 +0100

    correctedunit test conditions to pass

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -21,18 +21,22 @@ test('Works with single index arrays (hit)', () => {
 
   test('Works with multiple index arrays (hit)', () => {
     const coordinates = [25,24,23];
-    const name = "submarine" ;
-    const length = 1;
+    const name = "boat" ;
+    const length = 3;
     const submarine = createShip(name,length,coordinates);
     submarine.hit(23);
+    submarine.hit(24);
+    submarine.hit(25);
     expect( submarine.isSunk() ).toBe( true );
   });
 
   test('Works with multiple index arrays (miss)', () => {
     const coordinates = [25,24,23];
-    const name = "submarine" ;
-    const length = 1;
+    const name = "boat" ;
+    const length = 3;
     const submarine = createShip(name,length,coordinates);
+    submarine.hit(23);
+    submarine.hit(24);
     submarine.hit(29);
     expect( submarine.isSunk() ).toBe( false );
   });
