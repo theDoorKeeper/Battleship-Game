e2da12846a801a6c0c11f7ccd88b1fcee187245c
thteDoorKeeper <chakibou1@hotmail.com>
2021-07-06 18:06:39 +0100

    added 2 test cases for player module

--- src/test/player.test.js
+++ src/test/player.test.js
@@ -8,3 +8,23 @@ test('sinking a single ship when shipArray contains only one', () => {
 
     expect( testPlayer.hasLost() ).toBe( true );
   });
+
+
+  test('sinking a single ship when shipArray contains multipleOnes', () => {
+    const testPlayer = createPlayer("tester")
+    testPlayer.placeShip("submarine",22);
+    testPlayer.placeShip("battleship",10);
+    testPlayer.receiveAttack(22);
+    testPlayer.receiveAttack(23);
+
+    expect( testPlayer.hasLost() ).toBe( false );
+  });
+
+  test('missArray dispaying coordinates of unsunked ships', () => {
+    const testPlayer = createPlayer("tester")
+    testPlayer.placeShip("battleship",10);
+    testPlayer.receiveAttack(22);
+    testPlayer.receiveAttack(23);
+
+    expect( testPlayer.getMissArray() ).toEqual( [22,23] );
+  });
