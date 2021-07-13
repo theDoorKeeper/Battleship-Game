115b1146f0b58d63d19f46e512c25cb48d20a452
thteDoorKeeper <chakibou1@hotmail.com>
2021-07-14 00:32:42 +0100

    change the logic of get coodinates to only work backwards when it passes 100

--- src/Helper functions/getCoordinates.js
+++ src/Helper functions/getCoordinates.js
@@ -1,9 +1,7 @@
 const getCoordinates = (coordinate, length) =>{
     let coordinateArray =[];
 
-    if ( coordinate+length > 100 || coordinate+length > 90 || coordinate+length > 80 || coordinate+length > 70 || coordinate+length > 60 ||
-         coordinate+length > 50 ||oordinate+length > 40 || coordinate+length > 30 || coordinate+length > 20 || coordinate+length > 10 ){
-
+    if ( coordinate+length > 100 ){
         for (let i = 0; i < length; i++) {                  
             coordinateArray.push(coordinate - i)
         }
