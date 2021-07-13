84c2603cfa5a38df882f063acffd9bea1cfdbd98
thteDoorKeeper <chakibou1@hotmail.com>
2021-07-13 15:27:17 +0100

    removed getCoordinate method

--- src/JS/gameboard.js
+++ src/JS/gameboard.js
@@ -19,25 +19,6 @@ const createGameboard = () => {
         }
 }
 
-const getCoordinates = (coordinate, length) =>{
-    let coordinateArray =[];
-
-    if (coordinate+length > 100){
-
-        for (let i = 0; i < length; i++) {                  
-            coordinateArray.push(coordinate - i)
-        }
-
-    }
-    else {
-        for (let i = 0; i < length; i++) {                  
-            coordinateArray.push(coordinate + i)
-        }
-
-    }
-    return coordinateArray
-}
-
 const receiveAttack = (hitCoordinates) =>{
     let miss = true;
  
