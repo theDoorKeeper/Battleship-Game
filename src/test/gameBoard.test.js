c94126561338f03505bd6a509f3c3f25d1f78a82
thteDoorKeeper <chakibou1@hotmail.com>
2021-07-21 17:30:32 +0100

    changed test suits to suit the new coordinate logic

--- src/test/gameBoard.test.js
+++ src/test/gameBoard.test.js
@@ -2,17 +2,17 @@ import { createGameboard } from "../JS/gameboard";
 
 
 
-test('works with (0) coordinate', () => {
+test('works with (1) coordinate', () => {
   const testingGameboard = createGameboard();
-  testingGameboard.placeShip("submarine",0)
-  testingGameboard.receiveAttack(0);
+  testingGameboard.placeShip("submarine",1)
   testingGameboard.receiveAttack(1);
-  expect( testingGameboard.getHitArray() ).toEqual( [0,1] );
+  testingGameboard.receiveAttack(2);
+  expect( testingGameboard.getHitArray() ).toEqual( [1,2] );
 });
 
-test('works with (100) coordinate', () => {
+test('works with (97) coordinate', () => {
   const testingGameboard = createGameboard();
-  testingGameboard.placeShip("battleship",100)
+  testingGameboard.placeShip("battleship",97)
   testingGameboard.receiveAttack(100);
   testingGameboard.receiveAttack(99);
   testingGameboard.receiveAttack(98);
@@ -21,6 +21,18 @@ test('works with (100) coordinate', () => {
   expect( testingGameboard.getHitArray() ).toEqual( [100,99,98,97] );
 });
 
+test('works with (100) coordinate', () => {
+  const testingGameboard = createGameboard();
+  testingGameboard.placeShip("carrier",100)
+  testingGameboard.receiveAttack(100);
+  testingGameboard.receiveAttack(99);
+  testingGameboard.receiveAttack(98);
+  testingGameboard.receiveAttack(97);
+  testingGameboard.receiveAttack(96);
+  testingGameboard.receiveAttack(95);
+
+  expect( testingGameboard.getHitArray() ).toEqual( [100,99,98,97,96,95] );
+});
 
 
 
@@ -37,7 +49,7 @@ test('sinking a single ship when shipArray contains only one', () => {
 test('sinking a single ship when shipArray contains multipleOnes', () => {
   const testingGameboard = createGameboard();
   testingGameboard.placeShip("submarine",22);
-  testingGameboard.placeShip("battleship",10);
+  testingGameboard.placeShip("battleship",11);
   testingGameboard.receiveAttack(22);
   testingGameboard.receiveAttack(23);
 
@@ -47,44 +59,43 @@ test('sinking a single ship when shipArray contains multipleOnes', () => {
 test('sinking all the  ships when shipArray contains multipleOnes', () => {
   const testingGameboard = createGameboard();
   testingGameboard.placeShip("submarine",22);
-  testingGameboard.placeShip("battleship",10);
+  testingGameboard.placeShip("battleship",11);
   testingGameboard.receiveAttack(22);
   testingGameboard.receiveAttack(23);
-  testingGameboard.receiveAttack(10);
   testingGameboard.receiveAttack(11);
   testingGameboard.receiveAttack(12);
   testingGameboard.receiveAttack(13);
   testingGameboard.receiveAttack(14);
   testingGameboard.receiveAttack(15);
-
+  testingGameboard.receiveAttack(16);
 
   expect( testingGameboard.isGameLost() ).toBe( true );
 });
 
 test('missing shots correctly display', () => {
   const testingGameboard = createGameboard();
-  testingGameboard.placeShip("battleship",10);
+  testingGameboard.placeShip("battleship",5);
   testingGameboard.receiveAttack(22);
   testingGameboard.receiveAttack(23);
-  testingGameboard.receiveAttack(10);
+  testingGameboard.receiveAttack(5);
 
 
   expect( testingGameboard.missArray ).toEqual( [22,23] );
 });
+
 test('getHitArray returns the correct information', () => {
   const testingGameboard = createGameboard();
   testingGameboard.placeShip("submarine",22);
-  testingGameboard.placeShip("battleship",10);
+  testingGameboard.placeShip("battleship",11);
   testingGameboard.receiveAttack(22);
   testingGameboard.receiveAttack(23);
-  testingGameboard.receiveAttack(10);
   testingGameboard.receiveAttack(11);
   testingGameboard.receiveAttack(12);
   testingGameboard.receiveAttack(13);
   testingGameboard.receiveAttack(14);
   testingGameboard.receiveAttack(18);
   testingGameboard.receiveAttack(15);
+  testingGameboard.receiveAttack(16);
 
-
-  expect( testingGameboard.getHitArray() ).toEqual( [22,23,10,11,12,13] );
+  expect( testingGameboard.getHitArray() ).toEqual( [22,23,11,12,13,14] );
 });
--- src/test/player.test.js
+++ src/test/player.test.js
@@ -13,7 +13,7 @@ test('sinking a single ship when shipArray contains only one', () => {
   test('sinking a single ship when shipArray contains multipleOnes', () => {
     const testPlayer = createPlayer("tester")
     testPlayer.placeShip("submarine",22);
-    testPlayer.placeShip("battleship",10);
+    testPlayer.placeShip("battleship",11);
     testPlayer.receiveAttack(22);
     testPlayer.receiveAttack(23);
 
@@ -22,7 +22,7 @@ test('sinking a single ship when shipArray contains only one', () => {
 
   test('missArray dispaying coordinates of unsunked ships', () => {
     const testPlayer = createPlayer("tester")
-    testPlayer.placeShip("battleship",10);
+    testPlayer.placeShip("battleship",11);
     testPlayer.receiveAttack(22);
     testPlayer.receiveAttack(23);
 
@@ -31,24 +31,24 @@ test('sinking a single ship when shipArray contains only one', () => {
 
   test('hitArray dispaying hit coordinates of hit ships', () => {
     const testPlayer = createPlayer("tester")
-    testPlayer.placeShip("battleship",10);
-    testPlayer.receiveAttack(10);
+    testPlayer.placeShip("battleship",11);
     testPlayer.receiveAttack(11);
+    testPlayer.receiveAttack(12);
 
-    expect( testPlayer.getHitArray() ).toEqual( [10,11] );
+    expect( testPlayer.getHitArray() ).toEqual( [11,12] );
   });
 
 
   test('hitArray dispaying hit coordinates of mutliple ships', () => {
     const testPlayer = createPlayer("tester")
-    testPlayer.placeShip("battleship",10);
+    testPlayer.placeShip("battleship",11);
     testPlayer.placeShip("submarine",5);
-    testPlayer.receiveAttack(10);
     testPlayer.receiveAttack(11);
+    testPlayer.receiveAttack(12);
     testPlayer.receiveAttack(5);
     testPlayer.receiveAttack(6);
     testPlayer.receiveAttack(80);
 
-    expect( testPlayer.getHitArray() ).toEqual( [10,11,5,6] );
+    expect( testPlayer.getHitArray() ).toEqual( [11,12,5,6] );
   });
 
